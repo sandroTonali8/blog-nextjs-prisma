cf77d4425adfae7c796f255daf5d0f3eef5e6a65
Jimin <sjm1254245781@163.com>
2024-05-22 16:23:34 +0800

    feat: Add the function of creating article

---
+++ pages/api/post/index.ts
@@ -0,0 +1,20 @@
+import { getServerSession } from 'next-auth/next';
+import prisma from '../../../lib/prisma';
+import { authOptions } from '../auth/[...nextauth]';
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+
+  const session = await getServerSession(req, res, authOptions);
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
+}
